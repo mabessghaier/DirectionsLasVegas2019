eeaa0d21f6d8892cb64c3fe58abb27b21e84eda4
Carsten Howitz <chowitz@simcrest.com>
2019-04-24 17:53:51 -0400

    Scripts and Presentations

---
+++ Install Extension.ps1
@@ -0,0 +1,27 @@
+﻿# This should be run on the service tier
+
+# App name
+$AppName="App Name"
+$OldVersion= "1.0.0.0"
+$NewVersion = "1.0.0.1"
+
+# Change path and file name if needed
+$NewAppFile= "C:\apps\SimCrest_"+$AppName+"_"+$NewVersion+".app"
+# Change Server Instance to the instance you use
+$ServInstance= "BC130"
+
+# Change the path to the RTC client as needed
+Import-Module 'C:\Program Files (x86)\Microsoft Dynamics 365 Business Central\130\RoleTailored Client\NavModelTools.ps1' 
+
+# First uninstall old version
+UnInstall-NAVApp -ServerInstance $ServInstance -Name $AppName -Force
+UnPublish-NAVApp -ServerInstance $ServInstance -Name $AppName -Version $OldVersion
+# Upgrade to new version
+Publish-NAVApp -ServerInstance $ServInstance -Path $NewAppFile -SkipVerification
+Sync-NAVApp -ServerInstance $ServInstance -Name $AppName  # -Mode Development
+# Only use if -Mode Development is used
+# Install-NAVApp -ServerInstance $ServInstance -Name $AppName -Version $NewVersion
+Start-NavAppDataUpgrade -ServerInstance $ServInstance -Version $NewVersion -Name $AppName
+
+# List Apps on Service Tier
+Get-NAVAppInfo -ServerInstance $ServInstance
---
+++ Split and Compare to BC.ps1
@@ -0,0 +1,18 @@
+﻿# You may have to load this if running first time
+# Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+# Load Prerequisites
+Import-Module 'C:\Program Files (x86)\Microsoft Dynamics 365 Business Central\130\RoleTailored Client\NavModelTools.ps1' 
+
+# Export objects from from base line
+# Place original.txt as all baseline objects that are modified in MOD file
+# Place modified.txt as baseline plus all modified objects that needs to be in extension
+Split-NAVApplicationObjectFile -Source c:\al\original.txt -Destination 'c:\al\ORG' -Force
+Split-NAVApplicationObjectFile -Source C:\al\modified.txt -Destination 'c:\al\MOD' -Force
+
+# Compare
+Compare-NAVApplicationObject -OriginalPath c:\al\ORG -ModifiedPath c:\al\MOD -DeltaPath c:\al\DELTA -Force -ExportToNewSyntax
+
+# Create AL files
+cd 'C:\Program Files (x86)\Microsoft Dynamics 365 Business Central\130\RoleTailored Client'
+.\txt2al.exe --source=c:\al\delta --target=c:\al\al
